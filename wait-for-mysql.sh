e73ca66f81110bc7726f9871ffb126bd4daae27b
sejaltapadiya <155996001+sejaltapadiya@users.noreply.github.com>
2024-05-22 13:49:00 +0530

    Update wait-for-mysql.sh

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -1,17 +1,1 @@
-#!/bin/sh
-# wait-for-mysql.sh
-
-set -e
-
-host="$1"
-shift
-cmd="$@"
-
-until mysql -h "$host" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" -e "SELECT 1" > /dev/null 2>&1; do
-  >&2 echo "MySQL is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "MySQL is up - executing command"
-exec $cmd
 
